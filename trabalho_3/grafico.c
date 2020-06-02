cd9b6c7eeaea5511073cd9aaf5e9a43b39b584f1
Adriner Andrade <adriner_andrade@hotmail.com>
2020-06-02 19:44:00 -0300

    método gráfico

--- trabalho_3/grafico.c
+++ trabalho_3/grafico.c
@@ -58,10 +58,12 @@ unsigned char const pacman_right[] = {
 0x7F,0xE0,0x7F,0xC0,0x7F,0xFE,0x3F,0xFE,0x3F,0xFC,0x1F,0xF8,0x07,0xF0,0x00,0x00
 };
 
-void main() {
-  char txt1[] = " EINSTEIN WOULD HAVE LIKED mE";
-  char txt[] =  " GLCD LIBRARY DEMO, WELCOME !";
+//
+void print(unsigned char line, unsigned char column, code const unsigned char* sprite) {
+   T6963C_sprite((column - 1) * 16, (line - 1) * 16, sprite, 16, 16);
+}
 
+void main() {
   unsigned char  panel;         // Current panel
   unsigned int   i;             // General purpose register
 
@@ -71,6 +73,7 @@ void main() {
   TRISA4_bit = 1;               // Set RA4 as input
 
   // Initialize T6369C
+  // 15 x 8
   T6963C_init(240, 128, 8);
 
   /*
@@ -78,90 +81,7 @@ void main() {
    */
   T6963C_graphics(1);
   T6963C_text(1);
-  
-  T6963C_sprite(10,10,icone_16_16_pixel,16,16);
-  //T6963C_sprite(0,0,portaavioes,16,16);
-  
-  delay_ms(10000);
-  
-  T6963C_image(IMG_0046_bmp);
-  
-  delay_ms(3000);
-  
-  panel = 0;
-  i = 0;
-  /*
-   * Text messages
-   */
-  T6963C_write_text(txt, 0, 0, T6963C_ROM_MODE_XOR);
-  T6963C_write_text(txt1, 0, 15, T6963C_ROM_MODE_XOR);
 
-  /*
-   * Cursor
-   */
-  T6963C_cursor_height(8);       // 8 pixel height
-  T6963C_set_cursor(0, 0);       // Move cursor to top left
-  T6963C_cursor(0);              // Cursor off
-
-  /*
-   * Draw rectangles
-   */
-  T6963C_rectangle(0, 0, 239, 127, T6963C_WHITE);
-  T6963C_rectangle(20, 20, 219, 107, T6963C_WHITE);
-  T6963C_rectangle(40, 40, 199, 87, T6963C_WHITE);
-  T6963C_rectangle(60, 60, 179, 67, T6963C_WHITE);
-
-  /*
-   * Draw a cross
-   */
-  T6963C_line(0, 0, 239, 127, T6963C_WHITE);
-  T6963C_line(0, 127, 239, 0, T6963C_WHITE);
-  /*
-   * Draw solid boxes
-   */
-  T6963C_box(0, 0, 239, 8, T6963C_WHITE);
-  T6963C_box(0, 119, 239, 127, T6963C_WHITE);
-
-  /*
-   * Draw circles
-   */
-  T6963C_circle(120, 64, 10, T6963C_WHITE);
-  T6963C_circle(120, 64, 30, T6963C_WHITE);
-  T6963C_circle(120, 64, 50, T6963C_WHITE);
-  T6963C_circle(120, 64, 70, T6963C_WHITE);
-  T6963C_circle(120, 64, 90, T6963C_WHITE);
-  T6963C_circle(120, 64, 110, T6963C_WHITE);
-  T6963C_circle(120, 64, 130, T6963C_WHITE);
-
-  //T6963C_sprite(76, 4, einstein, 88, 119);         // Draw a sprite
-
-  T6963C_setGrPanel(1);                            // Select other graphic panel
-  
-  for(;;) {                                        // Endless loop
-
-    Opcao = RA3_bit * 2 + RA4_bit;
-
-    if(Opcao==0) {
-      T6963C_graphics(1);
-      T6963C_text(0);
-      Delay_ms(300);
-      }
-    else if(Opcao==1) {
-      panel++;
-      panel &= 1;
-      T6963C_displayGrPanel(panel);
-      Delay_ms(300);
-      }
-    else if(Opcao==2) {
-      T6963C_graphics(0);
-      T6963C_text(1);
-      Delay_ms(300);
-      }
-    else if(Opcao==3) {
-      T6963C_graphics(1);
-      T6963C_text(1);
-      Delay_ms(300);
-      }
-      delay_ms(2000);
-  }
+  print(1, 1, pacman_up);
+  print(1, 2, pacman_down);
 }
